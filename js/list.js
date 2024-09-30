9ed647be838dd7bd02b7cf11ac033543ce7d3863
XDRENGEN <149380097+XDRENGEN@users.noreply.github.com>
2024-09-30 17:54:25 +0200

    Rettet en fejl

--- js/list.js
+++ js/list.js
@@ -1,7 +1,22 @@
 
 console.log("Skriv dit Javascript her")
-let a = document.getElementsByTagName("li");
-console.log(a)
 
+// Dette returnere kun et element.
+let a = document.getElementById("one");
+console.log(a);
+
+let tagName = document.getElementsByTagName("li");
+console.log(tagName);
+
+
+
+let newLiElement = document.createElement("li")
+newLiElement.setAttribute("class", "hot")
+let newTextElement = document.createTextNode("Apple")
+newLiElement.appendChild(newTextElement)
+document.getElementById("firstul").append(newLiElement)
+
+let myClass = document.getElementByClassName("hot")
+console.log(myClass)
 
 console.log("slut på JavaScript")
